a1271419f1844685d579541fa72220c6d2b29c37
Raphael Isaac <isaacraphaelchidi@gmail.com>
2022-03-15 12:35:03 +0100

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,148 @@
+const parent = document.querySelector(".main-row")
+const image = document.querySelector(".dimage")
+const search = document.querySelector('#search')
+const select = document.querySelector('select')
+
+
+
+
+
+// window.addEventListener('load', ()=>{ 
+//     if
+//     poputateCountries()
+// })
+
+
+app();
+
+
+function app() {
+    if (parent.childElementCount === 0){
+        poputateCountries()
+
+        
+    }
+}
+
+search.addEventListener('keyup', (event)=>{
+    if(event.keyCode === 13){
+        event.preventDefault()
+        poputateCountries(search.value, "Country")
+        search.value = "";
+    }
+})
+
+// if(card){
+
+//     card.addEventListener('click', ()=>{
+//         console.log(card[2].innerText)
+//         localStorage.setItem('country', card[2].innerText)
+//     })
+// }
+
+
+select.addEventListener('change', ()=> {
+    poputateCountries(select.value, "Continent")
+})
+
+async function poputateCountries(text="",type=""){
+    
+    if(text.length && type === "Country")
+    {
+        image.classList.remove('noshow');
+        let response = await fetch(`https://restcountries.com/v2/name/${text}`)
+        let data = await response.json()
+        seedCountry(data)
+    } else if(text.length && type === "Continent")
+    {
+        image.classList.remove('noshow');
+        console.log('here')
+        let response = await fetch(`https://restcountries.com/v3.1/region/${text}`)
+        let data = await response.json()
+        seedContinent(data)
+    } else
+    {
+        let response = await fetch("https://restcountries.com/v2/all")
+        let data = await response.json()
+        seed(data)
+ 
+    }
+    
+
+}
+function myFunction(obj){
+    const cardBody = obj.firstElementChild.nextElementSibling
+    const next = document.querySelector('.nextbtn')
+    const countryName = cardBody.firstElementChild.innerText;
+    localStorage.clear()
+    localStorage.setItem('country', countryName)
+
+    next.click()
+}
+function seed(data) {
+    data.forEach(item => {
+        image.classList.add('noshow');
+        parent.innerHTML += `
+        <div class="col">
+        <div class="card" onclick="myFunction(this)">
+        
+            <img style="height:200px" src=${item.flags.png} class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${item.name}</h5>
+            <p class="card-text mt-2">
+            <strong>Population</strong> : ${item.population} <br>
+            <strong>Region</strong> : ${item.region} <br>
+            <strong>Capital</strong> : ${item.capital} <br>
+            </p>
+            </div>
+        </div>
+        </div>
+        `
+        
+    }); 
+}
+
+function seedCountry(data) {
+    data.forEach(item => {
+        image.classList.add('noshow');
+        parent.innerHTML = `
+        <div class="col">
+        <div class="card" onclick="myFunction()">
+        
+            <img style="height:200px" src=${item.flags.png} class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${item.name}</h5>
+            <p class="card-text mt-2">
+            <strong>Population</strong> : ${item.population} <br>
+            <strong>Region</strong> : ${item.region} <br>
+            <strong>Capital</strong> : ${item.capital} <br>
+            </p>
+            </div>
+        </div>
+        </div>
+        `
+})
+}
+function seedContinent(data) {
+    parent.innerHTML = ""
+    data.forEach(item => {
+        image.classList.add('noshow');
+        
+        parent.innerHTML += `
+        <div class="col">
+        <div class="card" onclick="myFunction()">
+        
+            <img style="height:200px" src=${item.flags.png} class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${item.name.common}</h5>
+            <p class="card-text mt-2">
+            <strong>Population</strong> : ${item.population} <br>
+            <strong>Region</strong> : ${item.region} <br>
+            <strong>Capital</strong> : ${item.capital} <br>
+            </p>
+            </div>
+        </div>
+        </div>
+        `
+})
+}
---
+++ scripts/theme.js
@@ -0,0 +1,26 @@
+// const btn = document.querySelector('.d-btn')
+// const body = document.querySelector('body')
+// const select = document.querySelector('select')
+// const option = document.querySelector('option')
+// const nav = document.querySelector('nav')
+// const searchInput = document.querySelector('#search')
+// const cardBody = document.querySelector('.card-body')
+// const card = document.querySelector('.card')
+
+// btn.addEventListener('click',()=> {
+//     toggleBody()
+//     toggleComponents()
+
+// })
+
+// function toggleBody(){
+//     body.classList.toggle('bdark')
+// }
+
+// function toggleComponents(){
+//     nav.classList.toggle('dark')
+//     select.classList.toggle('dark')
+//     option.classList.toggle('dark')
+//     searchInput.classList.toggle('dark')
+//     card.classList.toggle("dark")
+// }
